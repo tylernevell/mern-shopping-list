2a4574b42f81a4e7579c24ab9d889c78e872e381
Tyler Nevell <tylernevell@gmail.com>
2021-04-13 16:02:07 -0700

    created backend

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
+
 
 const app = express();
 
@@ -17,6 +20,9 @@ mongoose
     .connect(db, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log('MongoDB Connected.'))
     .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5000;
 
