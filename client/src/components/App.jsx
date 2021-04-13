8cb44666d575325610124c5b4a2b969bc1a7257a
Tyler Nevell <tylernevell@gmail.com>
2021-04-13 16:38:20 -0700

    Add react to project

---
+++ client/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return(
+        <div>
+            <h1>Helllo</h1>
+        </div>
+    );
+}
+
+export default App;
